oliliango/codeblocks

commit 86ebdb4d75db66fd54eaf5ca7cc2ef6261045eb4

* editor: added per language override for the 'use tabs' setting. Currently only for python (always use spaces) and Makefiles (always use tabs)
git-svn-id: http://svn.code.sf.net/p/codeblocks/code/trunk@7411 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/sdk/cbeditor.cpp
@@ -1188,6 +1188,23 @@ void cbEditor::SetEditorStyle()
     SetEditorStyleAfterFileOpen();
 }
 
+void OverrideUseTabsPerLanguage(cbStyledTextCtrl *control)
+{
+    if (!control)
+        return;
+    // override the use tab setting for Python files and Makefiles
+    int lexer = control->GetLexer();
+    switch (lexer)
+    {
+        case wxSCI_LEX_PYTHON:
+            control->SetUseTabs(false);
+            break;
+        case wxSCI_LEX_MAKEFILE:
+            control->SetUseTabs(true);
+            break;
+    }
+}
+
 void cbEditor::SetEditorStyleBeforeFileOpen()
 {
     ConfigManager* mgr = Manager::Get()->GetConfigManager(_T("editor"));
@@ -1219,6 +1236,9 @@ void cbEditor::SetEditorStyleBeforeFileOpen()
     SetFoldingIndicator(mgr->ReadInt(_T("/folding/indicator"), 2));
 
     SetLanguage( HL_AUTO );
+
+    OverrideUseTabsPerLanguage(m_pControl);
+    OverrideUseTabsPerLanguage(m_pControl2);
 }
 
 void cbEditor::SetEditorStyleAfterFileOpen()

commit 2c4e679bd68072a218c65e434a9680ec163a55fa

* editor: Fixed bug introduced with commit r7411 - adding new python file doesn't call the OverrideUseTabsPerLanguage and the global setting for use-tabs is used;

src/sdk/cbeditor.cpp
@@ -1790,6 +1790,8 @@ bool cbEditor::SaveAs()
     m_IsOK = true;
     SetModified(true);
     SetLanguage( HL_AUTO );
+    OverrideUseTabsPerLanguage(m_pControl);
+    OverrideUseTabsPerLanguage(m_pControl2);
     // store the last used filter and directory
     if (mgr)
     {